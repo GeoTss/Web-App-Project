364a8c9f6f2eb6b08086abaa4fbfb6b8e6a1c303
George_Tsorakos <giorgosuncharted@gmail.com>
2025-12-05 19:46:59 +0200

    Fixed register

---
+++ part-a-frontend/assets/js/forms/register-form.js
@@ -0,0 +1,63 @@
+import { CourseFilterLookup, FilterSectionManager, FiltersController, createFilterSection } from "../data/modules/filter-section.js"
+import { category_t } from "../data/modules/category-utils.js"
+import { difficulty_t } from "../data/modules/cards-utils.js"
+
+document.getElementById("password").addEventListener("input", e => {
+    const password = e.target.value;
+    document.getElementById("longer").textContent = password.length >= 8 ? " Is longer than 8 characters ✔" : " Is longer than 8 characters ✖";
+    document.getElementById("uppercase").textContent = /[A-Z]/.test(password) ? " Has uppercase ✔" : " Has uppercase ✖";
+    document.getElementById("lowercase").textContent = /[a-z]/.test(password) ? " Has lowercase ✔" : " Has lowercase ✖";
+    document.getElementById("number").textContent = /[0-9]/.test(password) ? " Has number ✔" : " Has number ✖";
+    document.getElementById("special").textContent = /[!@#$%^&*(),.?":{}|<>]/.test(password) ? " Has special character ✔" : " Has special character ✖";
+});
+
+document.getElementById("register-form").addEventListener("submit", e => {
+    e.preventDefault();
+
+    const debug = true;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const repeatPassword = document.getElementById("repeat-password").value;
+
+    if (!debug) {
+
+        if (password !== repeatPassword) {
+            alert("Passwords do not match.");
+            return;
+        }
+
+        let s = 0;
+        if (password.length >= 8) s++;
+        if (/[A-Z]/.test(password)) s++;
+        if (/[a-z]/.test(password)) s++;
+        if (/[0-9]/.test(password)) s++;
+        if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) s++;
+
+        if (s < 5) {
+            alert("Password too weak.");
+            return;
+        }
+    }
+
+    const encodedUser = btoa(JSON.stringify({ username, email, password }));
+
+    const encodedFilters = btoa(JSON.stringify(filters));
+    window.location.href = `courses.html?user=${encodedUser}&filters=${encodedFilters}`;
+});
+
+var filterManagers = [
+    new FilterSectionManager(CourseFilterLookup.DIFFICULTY, "difficulty", (itemInfo, checkmarkElem) => {
+        checkmarkElem.style.setProperty("--box-color", itemInfo.baseColor);
+    }),
+    new FilterSectionManager(CourseFilterLookup.CATEGORY, "category")
+];
+
+var filterController = new FiltersController(filterManagers);
+
+window.onload = () => {
+    createFilterSection(filterManagers);
+
+    filterController.populateManager(CourseFilterLookup.DIFFICULTY, Object.values(difficulty_t));
+    filterController.populateManager(CourseFilterLookup.CATEGORY, Object.values(category_t));
+}
