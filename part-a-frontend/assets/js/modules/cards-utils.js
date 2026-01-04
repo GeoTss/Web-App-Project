8afc2a80a48e960ffb045cea9b35d96c6db09983
TotoWasTaken <filippostotomis2@gmail.com>
2026-01-04 05:10:24 +0200

    ok?

--- part-a-frontend/assets/js/modules/cards-utils.js
+++
@@ -1,8 +0,0 @@
-export class codeMenuCard {
-    constructor(langName, desc, difficultyId, categoryId) {
-        this.langName = langName;
-        this.description = desc;
-        this.difficultyLookupId = difficultyId;
-        this.categoryLookupId = categoryId;
-    }
-}
